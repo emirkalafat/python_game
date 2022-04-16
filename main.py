c2cd45666b50f4cbd14871185e750a20d932456f
Emir Kalafat <emirklft@outlook.com>
2022-04-16 18:14:00 +0300

    ilk karakter tanımı

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import pygame
 
 pygame.init()
 ##Tanımlar
-renk1= (119,224,255)
+Rmavi= (119, 224, 255)
+Rsiyah= (0,0,0)
 GENISLIK = 500
 YUKSEKLIK = 700
-arkaplanRengi = renk1
-##
-oyuncu = pygame.image.load('karakter.png')
+arkaplanRengi = Rmavi
+oyuncu = pygame.transform.scale(pygame.image.load('karakter.png'),(80,80))
 fps = 60
 genel_font = pygame.font.Font('futurab.ttf',16)
 sayac = pygame.time.Clock()
 
-#EKRAN
+##Değişkenler
+karakter_konum_x = 170
+karakter_konum_y = 400
+platformlar = [[175,480,70,10]]
 
+#EKRAN
 ekran = pygame.display.set_mode([GENISLIK,YUKSEKLIK])
 pygame.display.set_caption('Emir Ebrar the Game')
 
@@ -21,6 +25,14 @@ calisiyorMu = True
 while calisiyorMu == True:
     sayac.tick(fps)
     ekran.fill(arkaplanRengi)
+    ekran.blit(oyuncu,(karakter_konum_x,karakter_konum_y))
+
+    taslar = []
+
+    for i in range(len(platformlar)):
+        tas = pygame.draw.rect(ekran,Rsiyah,platformlar[i])
+        taslar.append(tas)
+
 
     for olay in pygame.event.get():
         if olay.type == pygame.QUIT:
