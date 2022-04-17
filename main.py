c4a9a1fae3301505cb858288715b38328f7c3cf1
Emir Kalafat <emirklft@outlook.com>
2022-04-17 12:28:42 +0300

    karakterin zıplaması

--- main.py
+++ main.py
@@ -13,13 +13,41 @@ genel_font = pygame.font.Font('futurab.ttf',16)
 sayac = pygame.time.Clock()
 
 ##Değişkenler
-karakter_konum_x = 170
-karakter_konum_y = 400
-platformlar = [[175,480,70,10]]
+karakter_konum_x = 200
+karakter_konum_y = 550
+ilk_platform = [200,650,80,10]
+platformlar = [ilk_platform]
+zipliyorMu = False
+y_change = 0
+x_change = 0
+karakter_hizi = 5
 
 #EKRAN
 ekran = pygame.display.set_mode([GENISLIK,YUKSEKLIK])
 pygame.display.set_caption('Emir Ebrar the Game')
+
+#functions
+def karakteri_guncelle(konum_y):
+    global zipliyorMu
+    global y_change
+    ziplama_boyutu = 20
+    yercekimi = .5
+    if zipliyorMu:
+        y_change = -ziplama_boyutu
+        zipliyorMu = False
+    konum_y += y_change
+    y_change += yercekimi
+    return konum_y
+
+def tas_degiyorMu(tas_listesi, jump):
+    global karakter_konum_x
+    global karakter_konum_y
+    global y_change #karakter yukarı mı gidiyor aşağı mı gdiyor kontrolü
+    for i in range(len(tas_listesi)):
+        if tas_listesi[i].colliderect([karakter_konum_x, karakter_konum_y + 60, 90 ,10]) and jump is False and y_change > 0:
+            jump = True
+        return jump
+
 
 calisiyorMu = True
 while calisiyorMu == True:
@@ -27,16 +55,34 @@ while calisiyorMu == True:
     ekran.fill(arkaplanRengi)
     ekran.blit(oyuncu,(karakter_konum_x,karakter_konum_y))
 
+
     taslar = []
 
     for i in range(len(platformlar)):
-        tas = pygame.draw.rect(ekran,Rsiyah,platformlar[i])
+        tas = pygame.draw.rect(ekran,Rsiyah,platformlar[i],0,3)
         taslar.append(tas)
 
 
     for olay in pygame.event.get():
         if olay.type == pygame.QUIT:
             calisiyorMu = False
+
+        if olay.type == pygame.KEYDOWN:
+            if olay.key == pygame.K_a or olay.key == pygame.K_LEFT:
+                x_change = -karakter_hizi
+            if olay.key == pygame.K_d or olay.key == pygame.K_RIGHT:
+                x_change = karakter_hizi
+
+        if olay.type == pygame.KEYUP:
+            if olay.key == pygame.K_a or olay.key == pygame.K_LEFT:
+                x_change = 0
+            if olay.key == pygame.K_d or olay.key == pygame.K_RIGHT:
+                x_change = 0
+
+
+    zipliyorMu = tas_degiyorMu(taslar, zipliyorMu)
+    karakter_konum_x += x_change
+    karakter_konum_y = karakteri_guncelle(karakter_konum_y)
 
     pygame.display.flip()
 pygame.quit()
